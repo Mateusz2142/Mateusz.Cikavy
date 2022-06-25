80f97c51c6e6f82bb109d872a65410c68cc4e6c0
Mateusz <mateusz.cikavy@hotmail.com>
2022-06-25 14:34:51 +0200

    delete all

--- scripts/Datetime.py
+++
@@ -1,5 +0,0 @@
-print("Hello, what day is today?")
-from datetime import date
-print (date.today().strftime("%A"))
-print ("Have a nice day")
-input ("Press enter to continue")
--- scripts/drugi.py
+++
@@ -1,2 +0,0 @@
-print ('hello Agata')
-input (' press enter...')
